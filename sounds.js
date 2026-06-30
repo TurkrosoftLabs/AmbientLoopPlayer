71169f6c2e4bf034ff93c714124aae10eaced848
TurkrosoftLabs <qntkjz42n7@privaterelay.appleid.com>
2026-06-30 22:56:05 +0300

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,12 @@
+const sounds = [
+    "Birds and distant traffic.ogg",
+    "Rain on beach.ogg",
+    "Wind.ogg",
+    "Polar.ogg",
+    "Fireplace.ogg",
+    "Air conditioner.ogg",
+    "Desk fan.ogg",
+    "Car.ogg",
+    "Airplane.ogg",
+    "Aliens.ogg"
+]
